b65b92e549ddcbb1b4c4ac92780eb32d09d427b7
노승은 <seun2230@gmail.com>
2022-01-08 20:03:54 +0900

    모의(Mock) 함수 설정(2)

--- tests/example.js
+++ tests/example.js
@@ -1,8 +1,9 @@
-import axios from "axios";
-import _upperFirst from '/lodash/upperFirst'
-import _toLower from '/lodash/toLower'
+import axios from "axios"
+import _upperFirst from 'lodash/upperFirst'
+import _toLower from 'lodash/toLower'
 
-export async function fecthMovieTitle() {
+// 네트워크 오류 등 외부요인에 의해 테스트가 실패할 수 있다.
+export async function fetchMovieTitle() {
   const res = await axios.get('https://omdbapi.com?apikey=7035c60c&i=tt4520988')
   return _upperFirst(_toLower(res.data.Title))  // Frozen II => Frozen ii
 }
--- tests/example.test.js
+++ tests/example.test.js
@@ -1,27 +1,23 @@
-import * as example from "./example";
+import { fetchMovieTitle } from './example'
+import axios from 'axios'
 
-describe('async test', () => {
-  // test('done', (done) => {
-  //   asyncFunc().then(res => {
-  //     expect(res).toBe('Done!')  // 정상적으로 동작하지 않음
-  //     done()
-  //   })
-  // })
+// 모의 함수
+// 1. 테스트 외부 요인 제거 => 신뢰도 높임, 빠른 속도
+// 2. axios.get을 모의함수로 설정하여 원하는 데이터 반환하여 출력한다.
 
-  // test('then', () => {
-  //   return asyncFunc().then(res => {
-  //     expect(res).toBe('Done!')
-  //   })
-  // })
-
-  // test('resolve', () => expect(asyncFunc()).resolves.toBe('Done!'))
-  // test() 기본값은 최대 5초 .
-
-  // 모의 함수(Mock)
-  test('async/await', async () => {
-    jest.spyOn(example, 'asyncFunc')
-      .mockResolvedValue('Done!')
-    const res = await example.asyncFunc()
-    expect(res).toBe('Done!')
-  }, 7000)
+describe('비동기 테스트', () => {
+  test('영화 제목 변환', async () => {
+    // 모의 함수
+    axios.get = jest.fn(() => {
+      return new Promise(resolve => {  
+        resolve({  // 임의로 정한 데이터 반환
+          data: {
+            Title: 'Frozen II'
+          }
+        })
+      }) 
+    })
+    const title = await fetchMovieTitle()
+    expect(title).toBe('Frozen ii')
+  })
 })
