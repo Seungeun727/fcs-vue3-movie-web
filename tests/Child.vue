e84f50824b5b50b95749b9a9f4019a9e33b18a27
노승은 <seun2230@gmail.com>
2022-01-10 20:42:57 +0900

    shallowMount 학습

---
+++ tests/Child.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Child : {{ msg }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- tests/example.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-import _upperFirst from 'lodash/upperFirst'
-import _toLower from 'lodash/toLower'
-
-// 네트워크 오류 등 외부요인에 의해 테스트가 실패할 수 있다.
-export async function fetchMovieTitle() {
-  const res = await axios.get('https://omdbapi.com?apikey=7035c60c&i=tt4520988')
-  return _upperFirst(_toLower(res.data.Title))  // Frozen II => Frozen ii
-}
